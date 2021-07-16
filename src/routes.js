1f091f36124caf60774741bfa863d645df15d2af
Saachi Roye <saachi.roye@gmail.com>
2021-07-16 17:52:14 -0500

    added 404 page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 
 import Dashboard from './views/Dashboard';
+import NotFound from './views/404';
 
 const Routes = (props) => {
     return (
         <Switch>
             <Route exact path="/" component={Dashboard} />
+            <Route path="*" component={NotFound} />
         </Switch>
     );
 }
